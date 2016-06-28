fb6304b2dad9f0a819a70530f8e9a26959daaa49
equiva1ence <lawrence.matthew.bell@gmail.com>
2016-06-28 13:37:42 +0100

    Some refactoring
    Added a main.sublime-menu which allows us to add things to the dropdown
    menus in ST. Also added a .gitignore and a settings file.

--- context_build.py
+++ context_build.py
@@ -1,47 +1,58 @@
+# ------------------------------------------------------------------------------------------------------------------------------------------------- #
 import subprocess
-import argparse
 import platform
+import argparse
+import json
 import sys
 import os
 import re
 
-MAC_TEX_BIN_DIR = os.path.join(os.path.sep, "usr", "texbin")
-MAC_PDF_APP = "Skim"
-WIN_PDF_APP = "SumatraPDF"
-
 class PlatformNotRecognizedError(Exception):
   pass
 
-def parse():
+# ------------------------------------------------------------------------------------------------------------------------------------------------- #
+def parse_args():
   parser = argparse.ArgumentParser(description="builds ConTeXt files")
+  parser.add_argument("packages", metavar="p", type=str, help="the location of the packages folder")
   parser.add_argument("file", metavar="f", type=str, help="the (name of the) ConTeXt file to compile")
   return parser.parse_args()
 
-def parse_error(output, encoding="ascii"):
-  for line in reversed(output.decode(encoding).split("\n")):
-    if re.match(r"l\.\d+.*", line):
-      return "\n".join([
-        line if line[-1] not in {"\n", "\r"} else line[:-1],
-        "stopping"
-      ])
+def load_settings(packages_dir):
+  with open(os.path.join(packages_dir, "ConTeXtTools", "ConTeXtTools.sublime-settings")) as f:
+    return json.load(f)
 
-def get_pdf_name(tex_file):
-  return os.path.splitext(os.path.basename(tex_file))[0] + ".pdf"
-
-def open_pdf(tex_file):
+def prep_path(settings):
   if platform.system() == "Windows":
-    cmd = [WIN_PDF_APP, get_pdf_name(tex_file)]
+    pass
   elif platform.system() == "Darwin":
-    cmd = ["open", get_pdf_name(tex_file), "-a", MAC_PDF_APP]
+    tex_bin_path = os.path.join(os.path.sep, *settings.mac.tex_binaries_location)
+    os.environ["PATH"] = ":".join(os.environ["PATH"].split(":") + [tex_bin_path])
   else:
     raise PlatformNotRecognizedError("Operating system not recognized!")
-  subprocess.check_call(cmd)
 
+#def parse_error(output, encoding="ascii"):
+#  for line in reversed(output.decode(encoding).split("\n")):
+#    if re.match(r"l\.\d+.*", line):
+#      return "\n".join([
+#        line if line[-1] not in {"\n", "\r"} else line[:-1],
+#        "stopping"
+#      ])
+
+def open_pdf(tex_file, pdf_app):
+  pdf_name = os.path.splitext(os.path.basename(tex_file))[0] + ".pdf"
+  if platform.system() == "Windows":
+    cmd = [pdf_app, pdf_name]
+  elif platform.system() == "Darwin":
+    cmd = ["open", pdf_name, "-a", pdf_app]
+  else:
+    raise PlatformNotRecognizedError("Operating system not recognized!")
+  subprocess.call(cmd)
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------- #
 def main():
-  args = parse()
-  # append the location of all the tex binaries to PATH
-  os.environ["PATH"] = ":".join(os.environ["PATH"].split(":") + [MAC_TEX_BIN_DIR])
-  # call ConTeXt on the file
+  args = parse_args()
+  settings = load_settings(args.packages)
+  prep_path(settings)
   print("running ConTeXt...")
   sys.stdout.flush()
   process = subprocess.Popen(
@@ -64,7 +75,9 @@ def main():
     sys.stdout.flush()
     open_pdf(args.file)
   else:
-    print("error:", parse_error(out))
+    print("error:")#, parse_error(out))
+    print(out)
 
 if __name__ == "__main__":
-  main()
+  pass
+  #main()
