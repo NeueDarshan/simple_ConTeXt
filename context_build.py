48b5b217110af777826bcb246fd0c8af67dc15b2
equiva1ence <lawrence.matthew.bell@gmail.com>
2016-06-29 18:49:44 +0100

    tidying up
    Nothing much, tidied up some bits here and there, and added some
    comments throughout.

--- context_build.py
+++ context_build.py
@@ -1,84 +1,92 @@
-# ------------------------------------------------------------------------------------------------------------------------------------------------- #
 import subprocess
 import platform
 import argparse
 import json
 import sys
 import os
-import re
 
 class PlatformNotRecognizedError(Exception):
-  pass
+    pass
 
-# ------------------------------------------------------------------------------------------------------------------------------------------------- #
 def parse_args():
-  parser = argparse.ArgumentParser(description="builds ConTeXt files")
-  parser.add_argument("packages", metavar="p", type=str, help="the location of the packages folder")
-  parser.add_argument("file", metavar="f", type=str, help="the (name of the) ConTeXt file to compile")
-  return parser.parse_args()
+    parser = argparse.ArgumentParser(description="builds ConTeXt files")
+    parser.add_argument(
+        "packages",
+        metavar="p",
+        type=str,
+        help="the location of the packages folder")
+    parser.add_argument(
+        "file",
+        metavar="f",
+        type=str,
+        help="the (name of the) ConTeXt file to compile")
+    return parser.parse_args()
 
 def load_settings(packages_dir):
-  with open(os.path.join(packages_dir, "ConTeXtTools", "ConTeXtTools.sublime-settings")) as f:
-    return json.load(f)
+    with open(os.path.join(
+        packages_dir,
+        "ConTeXtTools",
+        "ConTeXtTools.sublime-settings")) \
+    as f:
+        return json.load(f)
 
 def prep_path(settings):
-  if platform.system() == "Windows":
-    pass
-  elif platform.system() == "Darwin":
-    tex_bin_path = os.path.join(os.path.sep, *settings["mac"]["tex_binaries_location"])
-    os.environ["PATH"] = ":".join(os.environ["PATH"].split(":") + [tex_bin_path])
-  else:
-    raise PlatformNotRecognizedError("Operating system not recognized!")
-
-#def parse_error(output, encoding="ascii"):
-#  for line in reversed(output.decode(encoding).split("\n")):
-#    if re.match(r"l\.\d+.*", line):
-#      return "\n".join([
-#        line if line[-1] not in {"\n", "\r"} else line[:-1],
-#        "stopping"
-#      ])
+    if platform.system() == "Windows":
+        pass
+    elif platform.system() == "Darwin":
+        tex_bin_path = os.path.join(
+            os.path.sep,
+            *settings["mac"]["tex_binaries_location"])
+        os.environ["PATH"] = ":".join(
+            os.environ["PATH"].split(":") + [tex_bin_path])
+    else:
+        raise PlatformNotRecognizedError("Operating system not recognized!")
 
 def open_pdf(tex_file, settings):
-  pdf_name = os.path.splitext(os.path.basename(tex_file))[0] + ".pdf"
-  if platform.system() == "Windows":
-    pdf_app = settings["windows"]["PDF_viewer"]
-    cmd = [pdf_app, pdf_name]
-  elif platform.system() == "Darwin":
-    pdf_app = settings["mac"]["PDF_viewer"]
-    cmd = ["open", pdf_name, "-a", pdf_app]
-  else:
-    raise PlatformNotRecognizedError("Operating system not recognized!")
-  subprocess.call(cmd)
+    pdf_name = os.path.splitext(os.path.basename(tex_file))[0] + ".pdf"
+    if platform.system() == "Windows":
+        pdf_app = settings["windows"]["PDF_viewer"]
+        cmd = [pdf_app, pdf_name]
+    elif platform.system() == "Darwin":
+        pdf_app = settings["mac"]["PDF_viewer"]
+        cmd = ["open", pdf_name, "-a", pdf_app]
+    else:
+        raise PlatformNotRecognizedError("Operating system not recognized!")
+    subprocess.call(cmd)
 
-# ------------------------------------------------------------------------------------------------------------------------------------------------- #
 def main():
-  args = parse_args()
-  settings = load_settings(args.packages)
-  prep_path(settings)
-  print("running ConTeXt...")
-  sys.stdout.flush()
-  process = subprocess.Popen(
-    ["context", args.file, "--synctex=1", "--noconsole=1"],
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE,
-    stderr=subprocess.STDOUT) # pipe stderr to stdout (not that we expect anything here, as context seems to only make use of stdout and not stderr)
-  out, _ = process.communicate()
-  # if the process still exists, make sure to close it
-  try:
-    process.terminate()
-  except OSError as e:
-    if e.errno == 3: # no such process, it's already closed
-      pass
-    else: # we've got the wrong guy... better re-raise the error then
-      raise e
-  # we're done now: if things worked then open the PDF, if not then kindly return the appropriate error message(s)
-  if process.returncode == 0:
-    print("success!")
+    # initial stuff
+    args = parse_args()
+    settings = load_settings(args.packages)
+    prep_path(settings)
+    print("running ConTeXt...")
     sys.stdout.flush()
-    open_pdf(args.file, settings)
-  else:
-    print("error:")#, parse_error(out))
-    print(out)
+    # run ConTeXt
+    process = subprocess.Popen(
+        ["context", args.file, "--synctex=1", "--noconsole=1"],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT)
+    out, _ = process.communicate()
+    # if the process still exists, make sure to close it
+    try:
+        process.terminate()
+    except OSError as e:
+        if e.errno == 3:  # no such process, it's already closed
+            pass
+        else:  # we've got the wrong guy... better re-raise the error then
+            raise e
+    # if things worked then open the PDF, if not then report what errors
+    # occurred
+    if process.returncode == 0:
+        print("success!")
+        sys.stdout.flush()
+        open_pdf(args.file, settings)
+    else:
+        print("error:")
+        # eventually let's parse the output and print the portion of it that
+        # describes the error... for now, simply print everything
+        print(out)
 
 if __name__ == "__main__":
-  main()
+    main()
