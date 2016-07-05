c681faa532399cbccfcc8828987b495bbb34534e
equiva1ence <lawrencebell@Lawrence-Matthew-Bells-MacBook-Pro.local>
2016-07-05 21:29:25 +0100

    Small Tidy
    Nothing much to say, aside from tidying up a few bits here and there,
    we’ve also added in functionality in syntax_pre_process.py to strip
    comments from the json (allowing us to put some comments in there) and
    from the sublime-syntax.pre files.

--- syntax_pre_process.py
+++ syntax_pre_process.py
@@ -2,46 +2,41 @@ import json
 import io
 import re
 
+
 def load_replacements():
-    # The tricks I know to make the syntax highlighting work (in Monokai
-    # Extended):
-    #   'string' - (light) yellow
-    #   'variable.parameter' - orange, italic
-    #   'keyword' - red
-    #   'markup.bold' - red, bold
-    #   'markup.italic' - red, italic
-    #   'markup.heading' - orange
-    #   'constant.(other|numeric|character|...)' - purple
-    #   'support.function' (or 'meta.diff'?) - blue
-    #   'entity' - green
-    #   'comment' - grey(ed out)
-    #   'support.entity' - green, italic
-    # Honestly though this facet of the syntax highlighting is the one I least
-    # understand... looking at the scopes that come up in markdown
-    # highlighting, I see a lot of interesting things.
-    #   If I get round to adding snippets and other such fancy things to
-    # ConTeXtTools, then I'll have to rework this stuff.
     with io.open("syntax_pre_process.json") as f:
-        return json.load(f)
+        input_str = f.read()
+        input_str = re.sub(
+            r"[ ]*//.*?\n",
+            r"",
+            input_str)
+        input_str = re.sub(
+            r"/\*.*?\*/\s*\n",
+            r"",
+            input_str,
+            flags=re.MULTILINE | re.DOTALL)
+        return json.loads(input_str)
+
 
 def pre_process(pre, post):
     repl = load_replacements()
-    with io.open(pre, mode="r", encoding="utf-8") as pre_file:
-        with io.open(post, mode="w", encoding="utf-8") as post_file:
-            for line in pre_file.readlines():
-                # It would be nice, although not really necessary, to clean up
-                # the comments while we're here.
-                #full_comment_pattern = r"\s*#.*"
-                #partial_comment_pattern = r"(.*?[^\\]+?(\\\\)*?)#.*"
-                #if re.match(full_comment_pattern, line):
-                #    continue
-                #elif re.search(partial_comment_pattern, line):
-                #    line = re.sub(partial_comment_pattern, r"\1", line)
-                for before, after in repl.items():
-                    replace_pattern = "{{@%s}}" % before
-                    if re.search(replace_pattern, line):
-                        line = re.sub(replace_pattern, after, line)
-                post_file.write(line)
+    with io.open(pre, mode="r") as pre_file:
+        with io.open(post, mode="w") as post_file:
+            input_str = pre_file.read()
+            input_str = re.sub(
+                r"^[ ]*#.*?\n",
+                r"",
+                input_str,
+                flags=re.MULTILINE)
+            input_str = re.sub(
+                r"^(.*?[^\\])[ ]*#.*?$",
+                r"\1",
+                input_str,
+                flags=re.MULTILINE)
+            for before, after in repl.items():
+                input_str = re.sub("{{@%s}}" % before, after, input_str)
+            post_file.write(input_str)
+
 
 def main():
     pre_process("ConTeXt.sublime-syntax.pre", "ConTeXt.sublime-syntax")
