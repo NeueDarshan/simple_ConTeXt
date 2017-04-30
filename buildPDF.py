72c7a5211134939401d90317e1b5dccdacce4293
equiva1ence <lawrence.matthew.bell@gmail.com>
2017-04-30 19:15:08 +0100

    Refactoring
    Adding to/tweaking the metadata that the main syntax file attaches to
    ConTeXt files. Updated the syntax test to reflect the current state of
    the syntax.
    
    On the scripting side of things, the new metadata allows for simplifying
    many bits and pieces. At the same time the code gets cleaned up a tad.
    
    Changed the `.sublime-menu` to use the `edit_settings` command, which
    seems like a better way of handling things.

--- buildPDF.py
+++ buildPDF.py
@@ -5,8 +5,14 @@ import time
 import os
 
 
+PACKAGE = os.path.abspath(
+    # os.path.join(sublime.packages_path(), "ConTeXtTools")
+    os.path.dirname(__file__)
+)
+
+
 import sys
-sys.path.insert(1, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(1, PACKAGE)
 from scripts import common
 
 
@@ -18,48 +24,51 @@ class ContextBuildPdfCommand(sublime_plugin.WindowCommand):
         start_time = time.time()
         self.reload_settings()
 
-        active_view = self.window.active_view()
-        if not common.is_context(active_view):
+        view = self.window.active_view()
+        if not common.is_context(view):
             return
 
         if not hasattr(self, "output_view"):
             self.output_view = self.window.get_output_panel("ConTeXtTools")
+
         self.output_view.settings().set("line_numbers", False)
         self.output_view.settings().set("gutter", False)
         self.output_view.settings().set("scroll_past_end", False)
         self.output_view.assign_syntax(
-            "Packages/ConTeXtTools/build results.sublime-syntax")
+            "Packages/ConTeXtTools/build results.sublime-syntax"
+        )
         self.output_view = self.window.get_output_panel("ConTeXtTools")
         self.window.run_command("show_panel", {"panel": "output.ConTeXtTools"})
 
-        dir_, input_ = os.path.split(active_view.file_name())
+        dir_, input_ = os.path.split(view.file_name())
         base = common.file_with_ext(input_, "")
         os.chdir(dir_)
 
-        context = self.current_profile.get("context_program", {})
-        command = context.get("name", "context")
-        options = context.get("options", {})
-        command = common.process_options(command, options, input_, base)
-
+        general = self.current_profile.get("context_program", {})
+        command = common.process_options(
+            general.get("name", "context"),
+            general.get("options", {}),
+            input_,
+            base
+        )
         chars = "running command: '{}'\n\n".format(" ".join(command))
         self.output_view.run_command("append", {"characters": chars})
 
-        path = self.current_profile.get("context_program", {}).get("path")
-        with common.ModPath(path):
-            context_process = subprocess.Popen(
+        with common.ModPath(general.get("path")):
+            process = subprocess.Popen(
                 command,
-                **{
-                    "std" + opt: subprocess.PIPE
-                    for opt in ["in", "out", "err"]
-                }
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
             )
 
-            context_process.communicate()
+            process.communicate()
             elapsed = time.time() - start_time
 
-            if context_process.returncode != 0:
+            if process.returncode != 0:
                 self.output_view.run_command("append", {"characters": "error"})
             else:
                 chars = "\n\n".join(
-                    ["Success!", "[Finished in {:.1f}s]".format(elapsed)])
+                    ["Success!", "[Finished in {:.1f}s]".format(elapsed)]
+                )
                 self.output_view.run_command("append", {"characters": chars})
--- references.py
+++ references.py
@@ -1,11 +1,17 @@
 import sublime
 import sublime_plugin
 import re
+import os
+
+
+PACKAGE = os.path.abspath(
+    # os.path.join(sublime.packages_path(), "ConTeXtTools")
+    os.path.dirname(__file__)
+)
 
 
 import sys
-import os
-sys.path.insert(1, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(1, PACKAGE)
 from scripts import common
 
 
@@ -27,36 +33,37 @@ class ContexttoolsReferenceSelector(sublime_plugin.WindowCommand):
             "references", {}).get(
                 "reference_regex", r"[a-zA-Z]+\:[a-zA-Z]+")
 
-        references = set()
+        refs = set()
         view = self.window.active_view()
 
-        definite_references = view.find_by_selector(
-            "text.tex.context meta.other.reference.context")
-        for region in definite_references:
+        definite_refs = view.find_by_selector("meta.other.reference.context")
+        for region in definite_refs:
             raw_ref = view.substr(region).strip()
             debraced_ref = re.match(r"\A{(.*?)}\Z", raw_ref)
             if debraced_ref:
-                references.add(debraced_ref.group(1).strip())
+                refs.add(debraced_ref.group(1).strip())
             else:
-                references.add(raw_ref.strip())
+                refs.add(raw_ref.strip())
 
-        potential_references = view.find_by_selector(
-            "text.tex.context meta.brackets.context"
-            " - meta.other.value.context")
-        for region in potential_references:
+        potential_refs = view.find_by_selector(
+            "meta.brackets.context - meta.other.value.context"
+        )
+        for region in potential_refs:
             raw_ref = view.substr(region)
             ref_match = re.match(
-                r"\A\s*\[(" + current_ref_regex + r")\]\s*\Z", raw_ref)
+                r"\A\s*\[(" + current_ref_regex + r")\]\s*\Z", raw_ref
+            )
             if ref_match:
-                references.add(ref_match.group(1).strip())
+                refs.add(ref_match.group(1).strip())
 
-        self.references = sorted(references)
+        self.references = sorted(refs)
 
     def run(self):
         if common.is_context(self.window.active_view()):
             self.reload_settings()
             self.window.show_quick_panel(
-                self.references, self.select_reference)
+                self.references, self.select_reference
+            )
 
     def select_reference(self, index):
         if 0 <= index < len(self.references):
@@ -66,25 +73,27 @@ class ContexttoolsReferenceSelector(sublime_plugin.WindowCommand):
             )
 
     def is_visible(self, *args):
-        return self.window.active_view().match_selector(0, "text.tex.context")
+        return common.is_context(self.window.active_view())
 
 
 class ContexttoolsReferenceMacroEventListener(sublime_plugin.EventListener):
     def reload_settings(self):
         common.reload_settings(self)
         self.current_cmd_regex = self.current_profile.get(
-            "references", {}).get(
-                "command_regex", "[a-zA-Z]*ref")
+            "references", {}).get("command_regex", "[a-zA-Z]*ref")
 
     def on_modified(self, view):
         self.reload_settings()
 
-        command_name, tail = common.last_command_in_region(
-            view, sublime.Region(0, view.sel()[0].end()))
+        end = view.sel()[0].end()
+        cmd = common.last_command_in_view(view, end=end)
+        if not cmd:
+            return
 
+        name = view.substr(cmd)[1:]
+        tail = view.substr(sublime.Region(cmd.end(), end))
         if (
-            command_name and
-            re.match(r"\A" + self.current_cmd_regex + r"\Z", command_name) and
+            re.match(r"\A" + self.current_cmd_regex + r"\Z", name) and
             re.match(r"\A[^\S\n]*\[[^\S\n]*\Z", tail)
         ):
             view.window().run_command("contexttools_reference_selector")
