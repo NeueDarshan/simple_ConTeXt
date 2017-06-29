e74aca5a119c7b5c1ecf4d8fe1c3edb6b219a821
SQbQxeKd3JHD8 <12873373+SQbQxeKd3JHD8@users.noreply.github.com>
2017-06-29 18:06:23 +0100

    Syntax corrections, new settings system
    Some small fixes to some `sublime-syntax` files. Besides that, starting
    on a new way of managing the settings, instead of the profile system.
    The motivation for this change is that the profile system, which is
    pretty good overall, is bad at making small changes; you have to make a
    whole separate profile if you want to, say, turn on the `--arrange` flag
    for compiling, or change the colours for the pop-ups.
    
    So the new approach is (will be) more straightforward: you can go into
    the settings in a nice GUI, using `quick_panels` provided by the ST API,
    and change whatever you want at will. It should also be able to emulate
    "profiles" as before: there will be a "scheme" feature (might rename
    better): schemes are simply a groups of settings, e.g. a scheme that
    dictates to use the interface file & engine for some-such version of
    context. Then you'll be able to select that scheme in the GUI, which
    will apply all the settings it contains. In line with all these changes,
    of course, the structure of the settings files will be changing some.
    
    That's the plan, anyway. This commit it just an initial attempt at
    implementing it. `commands.py` is where the GUI stuff is located, and
    the other python files have been made aware of the new system. The GUI
    is partially functional at this moment.

--- commands.py
+++ commands.py
@@ -1,7 +1,7 @@
 import sublime
 import sublime_plugin
-import json
 import collections
+import json
 import os
 
 
@@ -17,49 +17,258 @@ from scripts import common
 from scripts import parsing
 
 
-class ContexttoolsProfileSelector(sublime_plugin.WindowCommand):
+def _flatten(s, k, v):
+    if k == "interface":
+        return [k, ", ".join(sorted(s.get("interfaces", {})))]
+    elif k == "path":
+        return [k, ", ".join(sorted(s.get("program_paths", {})))]
+    elif k == "colour_schemes":
+        return [k, ", ".join(sorted(s.get("colour_schemes", {})))]
+    elif isinstance(v, str):
+        return [k, v]
+    elif hasattr(v, "__iter__"):
+        return [k, ", ".join(sorted(v))]
+    else:
+        return [k, str(v)]
+
+
+class ContexttoolsSettingsController(sublime_plugin.WindowCommand):
     def reload_settings(self):
         common.reload_settings(self)
 
     def run(self):
         self.reload_settings()
+        self.choices = {}
+        self._run_zero()
+
+    def _run_zero(self, selected_index=0):
         self.window.show_quick_panel(
-            self.profile_names,
-            self.select_profile,
-            0,
-            self.current_profile_index
+            sorted(
+                _flatten(self.sublime_settings, k, v)
+                for k, v in self.settings.items()
+            ),
+            self._run_one,
+            selected_index=selected_index,
         )
 
-    def select_profile(self, index):
-        if 0 <= index < len(self.profile_names):
-            self.settings.set("current_profile", self.profile_names[index])
+    def _run_one(self, index, selected_index=0):
+        dict_ = self.settings
+        if not 0 <= index < len(dict_):
+            return
+
+        key = sorted(dict_)[index]
+        val = dict_[key]
+        self.choices[0] = [index, key]
+
+        if key == "interface":
+            return self.window.show_quick_panel(
+                [["..", "↑ go back"]] +
+                sorted(
+                    [k, str(k == self.settings.get("interface"))]
+                    for k in self.interfaces
+                ),
+                self._run_two_interface,
+                selected_index=selected_index
+            )
+
+        self.window.show_quick_panel(
+            [["..", "↑ go back"]] +
+            sorted(
+                _flatten(self.sublime_settings, k, v) for k, v in val.items()
+            ),
+            self._run_two,
+            selected_index=selected_index
+        )
+
+    def _run_two_interface(self, index, selected_index=0):
+        if index == 0:
+            return self._run_zero(selected_index=self.choices[0][0])
+
+        index -= 1
+        dict_ = self.interfaces
+        if not 0 <= index < len(dict_):
+            return
+
+        key = sorted(dict_)[index]
+        self.choices[1] = [index, key]
+
+        self.settings["interface"] = key
+        self.sublime_settings.set("settings", self.settings)
+        sublime.save_settings("ConTeXtTools.sublime-settings")
+        self.reload_settings()
+        return self._run_one(self.choices[0][0], selected_index=index+1)
+
+    def _run_two(self, index, selected_index=0):
+        if index == 0:
+            return self._run_zero(selected_index=self.choices[0][0])
+
+        index -= 1
+        dict_ = self.settings[self.choices[0][1]]
+        if not 0 <= index < len(dict_):
+            return
+
+        key = sorted(dict_)[index]
+        val = dict_[key]
+        self.choices[1] = [index, key]
+
+        if isinstance(val, bool):
+            self.settings[self.choices[0][1]][key] = not val
+            self.sublime_settings.set("settings", self.settings)
             sublime.save_settings("ConTeXtTools.sublime-settings")
+            self.reload_settings()
+            return self._run_one(self.choices[0][0], selected_index=index+1)
+
+        elif isinstance(val, str):
+            if key == "path":
+                path = self.settings.get("program", {}).get("path")
+                return self.window.show_quick_panel(
+                    [["..", "↑ go back"]] +
+                    sorted([k, str(k == path)] for k in self.program_paths),
+                    self._run_three_path,
+                    selected_index=selected_index
+                )
+            else:
+                captions = {
+                    "name": "new program name",
+                    "command_regex": "new regex",
+                    "reference_regex": "new regex",
+                    "default": "new string"
+                }
+                return self.window.show_input_panel(
+                    captions.get(key, "default"),
+                    val,
+                    lambda string: self._run_three_str_done(
+                        string, selected_index=index+1
+                    ),
+                    self._run_three_str_change,
+                    lambda: self._run_three_str_cancel(selected_index=index+1),
+                )
+
+        self.window.show_quick_panel(
+            [["..", "↑ go back"]] +
+            sorted(
+                _flatten(self.sublime_settings, k, v) for k, v in val.items()
+            ),
+            self._run_three,
+            selected_index=selected_index
+        )
+
+    def _run_three_path(self, index, selected_index=0):
+        if index == 0:
+            return self._run_one(
+                1 + self.choices[1][0], selected_index=1+self.choices[1][0]
+            )
+
+        index -= 1
+        dict_ = self.program_paths
+        if not 0 <= index < len(dict_):
+            return
+
+        key = sorted(dict_)[index]
+        self.choices[2] = [index, key]
+
+        self.settings["program"]["path"] = key
+        self.sublime_settings.set("settings", self.settings)
+        sublime.save_settings("ConTeXtTools.sublime-settings")
+        self.reload_settings()
+        return self._run_two(1 + self.choices[1][0], selected_index=index+1)
+
+    def _run_three_str_change(self, string):
+        pass
+
+    def _run_three_str_cancel(self, selected_index=0):
+        return self._run_one(self.choices[0][0], selected_index=selected_index)
+
+    def _run_three_str_done(self, string, selected_index=0):
+        self.settings[self.choices[0][1]][self.choices[1][1]] = string
+        self.sublime_settings.set("settings", self.settings)
+        sublime.save_settings("ConTeXtTools.sublime-settings")
+        self.reload_settings()
+        return self._run_one(
+            self.choices[0][0], selected_index=selected_index
+        )
+
+    def _run_three(self, index, selected_index=0):
+        if index == 0:
+            return self._run_one(1 + self.choices[1][0])
+
+        index -= 1
+        dict_ = self.settings[self.choices[0][1]][self.choices[1][1]]
+        if not 0 <= index < len(dict_):
+            return
+
+        key = sorted(dict_)[index]
+        val = dict_[key]
+        self.choices[2] = [index, key]
+
+        if isinstance(val, bool):
+            self.settings[self.choices[0][1]][self.choices[1][1]][key] = \
+                not val
+            self.sublime_settings.set("settings", self.settings)
+            sublime.save_settings("ConTeXtTools.sublime-settings")
+            self.reload_settings()
+            return self._run_two(
+                1 + self.choices[1][0], selected_index=index+1
+            )
+
+        # elif isinstance(val, str):
+        #     if key == "path":
+        #         path = self.settings.get("program", {}).get("path")
+        #         return self.window.show_quick_panel(
+        #             [["..", "↑ go back"]] +
+        #             sorted([k, str(k == path)] for k in self.program_paths),
+        #             self._run_three_path,
+        #             selected_index=selected_index
+        #         )
+        #     else:
+        #         captions = {
+        #             "name": "new program name",
+        #             "command_regex": "new regex",
+        #             "reference_regex": "new regex",
+        #             "default": "new string"
+        #         }
+        #         return self.window.show_input_panel(
+        #             captions.get(key, "default"),
+        #             val,
+        #             lambda string: self._run_three_str_done(
+        #                 string, selected_index=index+1
+        #             ),
+        #             self._run_three_str_change,
+        #             lambda: self._run_three_str_cancel(selected_index=index+1),
+        #         )
+
+        # self.window.show_quick_panel(
+        #     [["..", "↑ go back"]] +
+        #     sorted(
+        #         _flatten(self.sublime_settings, k, v) for k, v in val.items()
+        #     ),
+        #     self._run_three,
+        #     selected_index=selected_index
+        # )
 
 
 class ContexttoolsGenerateInterface(sublime_plugin.WindowCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.interface_names = []
 
     def reload_settings(self):
         common.reload_settings(self)
-        self.interface_names = sorted(self.interfaces)
 
     def run(self):
         self.reload_settings()
         self.window.show_quick_panel(
-            self.interface_names, self.generate_interface
+            sorted(self.interfaces), self.generate_interface
         )
 
     def generate_interface(self, index):
         if not (0 <= index < len(self.interface_names)):
             return
 
-        name = self.interface_names[index]
+        name = sorted(s for s in self.interfaces)[index]
         interface = self.interfaces[name]
-
         main = interface.get("main")
         modules = interface.get("modules")
+
         if not main:
             return
 
@@ -96,14 +305,15 @@ class ContexttoolsQueryInterfaceCommands(sublime_plugin.WindowCommand):
 
         names = []
         for file in os.listdir(os.path.join(PACKAGE, "interface")):
-            if file.endswith(".json") and file.startswith("commands "):
+            if file.startswith("commands ") and file.endswith(".json"):
                 names.append(file[9:-5])
         names = sorted(names)
         for name in sorted(names):
             self.json_files[name] = []
 
         self.window.show_quick_panel(
-            list(self.json_files.keys()), self._run_choose)
+            list(self.json_files.keys()), self._run_choose
+        )
 
     def _run_choose(self, index):
         if not (0 <= index < len(self.json_files)):
@@ -121,8 +331,10 @@ class ContexttoolsQueryInterfaceCommands(sublime_plugin.WindowCommand):
                     self.json_files[name].append({"details": var, "name": cmd})
             self.window.show_quick_panel(
                 [
-                    "\\{} ".format(var["name"]) +
-                    " ".join(d["rendering"] for d in var["details"])
+                    "\\{} {}".format(
+                        var["name"],
+                        " ".join(d["rendering"] for d in var["details"])
+                    )
                     for var in self.json_files[name]
                 ],
                 self._run_print,
@@ -135,9 +347,10 @@ class ContexttoolsQueryInterfaceCommands(sublime_plugin.WindowCommand):
             self.window.run_command(
                 "contexttools_interface_command_insert",
                 {
-                    "command":
-                        "\\{} ".format(var["name"]) +
+                    "command": "\\{} {}".format(
+                        var["name"],
                         " ".join(d["rendering"] for d in var["details"])
+                    )
                 }
             )
 
