f4fa151c1efa68b3bfe87b3282acf60357e8be10
SQbQxeKd3JHD8 <12873373+SQbQxeKd3JHD8@users.noreply.github.com>
2017-07-09 22:45:46 +0100

    fixes & experimenting with phantoms
    Corrected using the old name in the settings. Added a few commands to `ConTeXt.sublime-syntax`. The biggest thing is experimenting with phantoms: some CSS styling could be nice. At the moment, a plain-looking phantom appears after running a build at each error, if any.

--- buildPDF.py
+++ buildPDF.py
@@ -3,6 +3,7 @@ import sublime_plugin
 import subprocess
 import threading
 import time
+import html
 import os
 
 
@@ -33,6 +34,7 @@ class SimpleContextBuildPdfCommand(sublime_plugin.WindowCommand):
         common.reload_settings(self)
 
     def run(self):
+        self.hide_phantoms()
         if self.state == 0:
             self.start_run()
         else:
@@ -64,16 +66,17 @@ class SimpleContextBuildPdfCommand(sublime_plugin.WindowCommand):
         self.start_time = time.time()
 
         self.reload_settings()
-        view = self.window.active_view()
-        if not common.is_context(view):
+        self.view = self.window.active_view()
+        if not common.is_context(self.view):
             return
 
-        if view.is_dirty():
-            view.run_command("save")
+        if self.view.is_dirty():
+            self.view.run_command("save")
 
         self.setup_output_view()
+        self.phantom_set = sublime.PhantomSet(self.view, "simple_context")
 
-        dir_, input_ = os.path.split(view.file_name())
+        dir_, input_ = os.path.split(self.view.file_name())
         self.base = common.file_with_ext(input_, "")
         os.chdir(dir_)
 
@@ -102,52 +105,92 @@ class SimpleContextBuildPdfCommand(sublime_plugin.WindowCommand):
         chars += "\n"
         self.output_view.run_command("append", {"characters": chars})
 
-        thread = threading.Thread(target=lambda: self._run(path))
+        thread = threading.Thread(target=lambda: self._start_run(path))
         thread.start()
 
-    def _run(self, path):
+    def _start_run(self, path):
         self.lock.acquire()
         if self.cancel:
             self.state = 0
             self.cancel = False
             return
 
-        self.state = 2
         flags = CREATE_NO_WINDOW if sublime.platform() == "windows" else 0
+        opts = {
+            "creationflags": flags,
+            "stdin": subprocess.PIPE,
+            "stdout": subprocess.PIPE,
+            "stderr": subprocess.PIPE,
+        }
+        if path:
+            orig_path = os.environ["PATH"]
+            os.environ["PATH"] = common.add_path(orig_path, path)
+            opts["env"] = os.environ.copy()
 
-        with common.ModPath(path):
-            self.process = subprocess.Popen(
-                self.command,
-                creationflags=flags,
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE
-            )
-            self.lock.release()
+        self.state = 2
+        self.process = subprocess.Popen(self.command, **opts)
+        self.lock.release()
+
+        if path:
+            os.environ["PATH"] = orig_path
 
         result = self.process.communicate()
-        elapsed = time.time() - self.start_time
-        log = common.parse_log(result[0])
-        builder = self.settings.get("builder", {})
+        self.elapsed = time.time() - self.start_time
+        self.log = common.parse_log(result[0])
+        self.builder = self.settings.get("builder", {})
+
+        self.process_errors(flags)
+        self.do_phantoms()
+
+        self.process = None
+        self.state = 0
+
+    def do_phantoms(self):
+        if self.builder.get("show_phantom_errors"):
+            self.phantom_set.update([
+                sublime.Phantom(
+                    sublime.Region(self.view.text_point(int(e["line"]) - 1, 0)),
+                    (
+                        html.escape(
+                            self.parse_error(e, verbose=False), quote=False
+                        ) + ' <a href="hide">[×]</a>'
+                    ),
+                    sublime.LAYOUT_BLOCK,
+                    on_navigate=self.hide_phantoms
+                )
+                for e in self.log["errors"]
+            ])
+
+    def hide_phantoms(self, href="hide"):
+        if hasattr(self, "view"):
+            self.view.erase_phantoms("simple_context")
+
+    def parse_error(self, e, verbose=True):
+        if verbose:
+            if e["line"] and e["details"]:
+                return "error    > {error} > line {line}: {details}".format(**e)
+            elif e["details"]:
+                return "error    > {error} > {details}".format(**e)
+            elif e["line"]:
+                return "error    > {error} > line {line}".format(**e)
+            else:
+                return "error    > {error} >".format(**e)
+        else:
+            if e["details"]:
+                return "{error}: {details}".format(**e)
+            else:
+                return e["error"]
+
+    def process_errors(self, flags):
         chars = ""
 
-        if builder.get("show_warnings"):
-            for d in log["warnings"]:
+        if self.builder.get("show_warnings"):
+            for d in self.log["warnings"]:
                 chars += "\nwarning  > {type} > {message}".format(**d)
 
-        if builder.get("show_errors"):
-            for e in log["errors"]:
-                if e["line"] and e["details"]:
-                    chars += (
-                        "\nerror    > {error} > line {line}: {details}"
-                        .format(**e)
-                    )
-                elif e["details"]:
-                    chars += "\nerror    > {error} > {details}".format(**e)
-                elif e["line"]:
-                    chars += "\nerror    > {error} > line {line}".format(**e)
-                else:
-                    chars += "\nerror    > {error} >".format(**e)
+        if self.builder.get("show_errors"):
+            for e in self.log["errors"]:
+                chars += "\n" + self.parse_error(e)
 
         if len(chars) > 0:
             self.output_view.run_command("append", {"characters": chars})
@@ -156,28 +199,29 @@ class SimpleContextBuildPdfCommand(sublime_plugin.WindowCommand):
             chars = ""
 
         if self.process.returncode == 0:
-            if builder.get("show_pages") and log.get("pages"):
+            if self.builder.get("show_pages") and self.log.get("pages"):
                 chars += (
-                    "\nsuccess  > shipped {} page{}"
-                    .format(log["pages"], "" if log["pages"] == 1 else "s")
+                    "\nsuccess  > shipped {} page{}".format(
+                        self.log["pages"],
+                        "" if self.log["pages"] == 1 else "s"
+                    )
                 )
-            viewer = builder.get("viewer")
-            if viewer and builder.get("open_PDF"):
+            viewer = self.builder.get("viewer")
+            if viewer and self.builder.get("open_PDF"):
                 chars += "\nsuccess  > opening PDF with {}".format(viewer)
                 subprocess.Popen(
                     [viewer, "{}.pdf".format(self.base)], creationflags=flags
                 )
-            chars += "\nsuccess  > finished in {:.1f}s".format(elapsed)
+            chars += "\nsuccess  > finished in {:.1f}s".format(self.elapsed)
         else:
-            chars += "\nfailure  > finished in {:.1f}s".format(elapsed)
+            chars += "\nfailure  > finished in {:.1f}s".format(self.elapsed)
 
         self.output_view.run_command("append", {"characters": chars})
-        self.process = None
-        self.state = 0
 
     def setup_output_view(self):
         if not hasattr(self, "output_view"):
-            self.output_view = self.window.get_output_panel("simple_ConTeXt")
+            self.output_view = \
+                self.window.create_output_panel("simple_ConTeXt")
 
         self.output_view.settings().set("line_numbers", False)
         self.output_view.settings().set("gutter", False)
@@ -186,7 +230,7 @@ class SimpleContextBuildPdfCommand(sublime_plugin.WindowCommand):
         self.output_view.assign_syntax(
             "Packages/simple_ConTeXt/build results.sublime-syntax"
         )
-        self.output_view = self.window.get_output_panel("simple_ConTeXt")
+        self.output_view = self.window.create_output_panel("simple_ConTeXt")
         self.window.run_command(
             "show_panel", {"panel": "output.simple_ConTeXt"}
         )
