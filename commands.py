f58cbe2ad8d886278a2de2ca1bb90ab505197b69
equiva1ence <lawrence.matthew.bell@gmail.com>
2017-07-27 22:52:35 +0100

    doc subtitles

--- commands.py
+++ commands.py
@@ -13,37 +13,36 @@ from .scripts import interface_writing as writing
 CREATE_NO_WINDOW = 0x08000000
 
 DOCS = [
-    "about",
-    "charts-mkiv",
-    "colors-mkiv",
-    "columnsets",
-    "details",
-    "epub-mkiv",
-    "epub-mkiv-demo",
-    "hybrid",
-    "languages-mkiv",
-    "lua-mkiv",
-    "luatex",
-    "ma-cb-en",
-    "math-mkiv",
-    "mk",
-    "mmlexamp",
-    "mmlprime",
-    "mreadme",
-    "rules-mkiv",
-    "spacing-mkiv",
-    "spreadsheets-mkiv",
-    "sql-mkiv",
-    "steps-mkiv",
-    "still",
-    "swiglib-mkiv",
-    "templates-mkiv",
-    "tiptrick",
-    "tools-mkiv",
-    "units-mkiv",
-    "workflows-mkiv",
-    "xml-mkiv",
-    "xtables-mkiv",
+    ["about", "LuaTeX and ConTeXt"],
+    ["charts-mkiv", "Flowcharts"],
+    ["colors-mkiv", "Coloring ConTeXt"],
+    ["columnsets", "Columns"],
+    ["details", "It's In The Details"],
+    ["epub-mkiv", "Exporting XML and EPUB from ConTeXt"],
+    ["hybrid", "MkIV Hybrid Technology"],
+    ["languages-mkiv", "Languages in ConTeXt"],
+    ["lua-mkiv", "The ConTeXt Libraries"],
+    ["luatex", "LuaTeX Reference Manual"],
+    ["ma-cb-en", "ConTeXt MkIV: An Excursion"],
+    ["math-mkiv", "Math"],
+    ["mk", "The History of LuaTeX"],
+    ["mmlexamp", "MathML Examples"],
+    ["mmlprime", "MathML"],
+    ["mreadme", "README"],
+    ["rules-mkiv", "Rules"],
+    ["spacing-mkiv", "Spacing in ConTeXt"],
+    ["spreadsheets-mkiv", "Simple Spreadsheets"],
+    ["sql-mkiv", "SQL"],
+    ["steps-mkiv", "ConTeXt XML"],
+    ["still", "Still Going On"],
+    ["swiglib-mkiv", "SwigLib Basics"],
+    ["templates-mkiv", "LMX Templates"],
+    ["tiptrick", "Tips and Tricks"],
+    ["tools-mkiv", "Luatools, Mtxrun, Context"],
+    ["units-mkiv", "Units"],
+    ["workflows-mkiv", "Workflow Support In ConTeXt"],
+    ["xml-mkiv", "Dealing With XML In ConTeXt"],
+    ["xtables-mkiv", "Extreme Tables"],
 ]
 
 
@@ -130,14 +129,14 @@ class SimpleContextFindDocs(sublime_plugin.WindowCommand):
 
     def run_panel(self, selected_index=0):
         self.window.show_quick_panel(
-            self.docs + ["... look for other file"],
+            self.docs + [["...", "search for other files"]],
             self.run_handle,
             selected_index=selected_index
         )
 
     def run_handle(self, index):
         if 0 <= index < len(self.docs):
-            self.open_doc(self.docs[index])
+            self.open_doc(self.docs[index][0])
         elif index == len(self.docs):
             self.window.show_input_panel(
                 "name", "", self.on_done, self.on_change, self.on_cancel,
@@ -154,6 +153,5 @@ class SimpleContextFindDocs(sublime_plugin.WindowCommand):
 
     def open_doc(self, name):
         file = utilities.locate(self._path, "{}.pdf".format(name))
-        print()
-        if self.viewer:
+        if self.viewer and file:
             subprocess.Popen([self.viewer, file], creationflags=self.flags)
